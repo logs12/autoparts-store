d94be5102a905d4bdef5665af87843b4a61a0189
Vasiliy <vasiliys492@gmail.com>
2016-10-10 00:10:46 +0300

    1

--- app/src/components/SearchResult/index.js
+++ app/src/components/SearchResult/index.js
@@ -15,23 +15,21 @@ export default class SearchResult extends Component {
         let originalProducts = props.data.map((item, index) => {
             if (item.group == 'Original') {
                 return (
-                    <div className="search-result__container" key={index}>
-                        <div className="search-result__row">
-                            <div className="search-result__col">
-                                {item.manufacturer} {item.articul}
-                            </div>
-                            <div className="search-result__col">
-                                {item.name}
-                            </div>
-                            <div className="search-result__col">
-                                {item.num} шт.
-                            </div>
-                            <div className="search-result__col">
-                                {item.term} дн.
-                            </div>
-                            <div className="search-result__col">
-                                {item.price} руб.
-                            </div>
+                    <div className="search-result__row" key={index}>
+                        <div className="search-result__col">
+                            {item.manufacturer} {item.articul}
+                        </div>
+                        <div className="search-result__col">
+                            {item.name}
+                        </div>
+                        <div className="search-result__col">
+                            {item.num} шт.
+                        </div>
+                        <div className="search-result__col">
+                            {item.term} дн.
+                        </div>
+                        <div className="search-result__col">
+                            {item.price} руб.
                         </div>
                     </div>
                 );
@@ -44,13 +42,17 @@ export default class SearchResult extends Component {
         return(
             <div className="search-result">
                 <div className="search-result__header">
-                    <div className="search-result__col">ПРОИЗВОДИТЕЛИ</div>
-                    <div className="search-result__col">НАИМЕНОВАНИЕ</div>
-                    <div className="search-result__col">В НАЛИЧИИ</div>
-                    <div className="search-result__col">СРОК ОЖИДАНИЯ</div>
-                    <div className="search-result__col">ЦЕНА</div>
+                    <div className="search-result__row">
+                        <div className="search-result__col">ПРОИЗВОДИТЕЛИ</div>
+                        <div className="search-result__col">НАИМЕНОВАНИЕ</div>
+                        <div className="search-result__col">В НАЛИЧИИ</div>
+                        <div className="search-result__col">СРОК ОЖИДАНИЯ</div>
+                        <div className="search-result__col">ЦЕНА</div>
+                    </div>
                 </div>
-                {this.renderContentOriginal(this.props)}
+                <div className="search-result__container">
+                    {this.renderContentOriginal(this.props)}
+                </div>
             </div>
         )
     }
