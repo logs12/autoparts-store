6b3333dc355c8b04021f7883d4ea956ce06d996b
Vasiliy <vasiliys492@gmail.com>
2016-10-14 00:49:17 +0300

    1

--- app/src/components/SearchResult/index.js
+++ app/src/components/SearchResult/index.js
@@ -20,22 +20,22 @@ export default class SearchResult extends Component {
             if (item.group == 'Original') {
                 return (
                     <div className="content-grid mdl-grid" key={index}>
-                        <div className="mdl-cell mdl-cell--3-col mdl-cell--2-col-tablet">
+                        <div className="mdl-cell mdl-cell--3-col mdl-cell--2-col-tablet mdl-cell--4-col-phone">
                             {item.manufacturer} {item.articul}
                         </div>
-                        <div className="mdl-cell mdl-cell--2-col mdl-cell--2-col-tablet">
+                        <div className="mdl-cell mdl-cell--2-col mdl-cell--1-col-tablet mdl-cell--4-col-phone">
                             {item.name}
                         </div>
-                        <div className="mdl-cell mdl-cell--2-col mdl-cell--1-col-tablet">
+                        <div className="mdl-cell mdl-cell--2-col mdl-cell--1-col-tablet mdl-cell--4-col-phone">
                             {item.num} шт.
                         </div>
-                        <div className="mdl-cell mdl-cell--2-col mdl-cell--1-col-tablet">
+                        <div className="mdl-cell mdl-cell--2-col mdl-cell--1-col-tablet mdl-cell--4-col-phone">
                             {item.term} дн.
                         </div>
-                        <div className="mdl-cell mdl-cell--2-col mdl-cell--1-col-tablet">
+                        <div className="mdl-cell mdl-cell--1-col mdl-cell--1-col-tablet mdl-cell--4-col-phone">
                             {item.price} руб.
                         </div>
-                        <div className="mdl-cell mdl-cell--1-col mdl-cell--1-col-tablet">
+                        <div className="mdl-cell mdl-cell--2-col mdl-cell--2-col-tablet mdl-cell--4-col-phone">
                             <DialogCallback />
                         </div>
                     </div>
@@ -56,15 +56,16 @@ export default class SearchResult extends Component {
                         <Paper style={style} zDepth={1} >
                             <div className="search-result__header">
                                 <div className="content-grid mdl-grid">
-                                    <div className="mdl-cell mdl-cell--3-col mdl-cell--2-col-tablet">ПРОИЗВОДИТЕЛИ</div>
-                                    <div className="mdl-cell mdl-cell--2-col mdl-cell--2-col-tablet">НАИМЕНОВАНИЕ</div>
-                                    <div className="mdl-cell mdl-cell--2-col mdl-cell--1-col-tablet">В НАЛИЧИИ</div>
-                                    <div className="mdl-cell mdl-cell--2-col mdl-cell--1-col-tablet">СРОК ОЖИДАНИЯ</div>
-                                    <div className="mdl-cell mdl-cell--2-col mdl-cell--1-col-tablet">ЦЕНА</div>
-                                    <div className="mdl-cell mdl-cell--1-col mdl-cell--1-col-tablet"></div>
+                                    <div className="mdl-cell mdl-cell--3-col mdl-cell--2-col-tablet mdl-cell—hide-phone ">ПРОИЗВОДИТЕЛИ</div>
+                                    <div className="mdl-cell mdl-cell--2-col mdl-cell--1-col-tablet mdl-cell—hide-phone ">НАИМЕНОВАНИЕ</div>
+                                    <div className="mdl-cell mdl-cell--2-col mdl-cell--1-col-tablet mdl-cell—hide-phone  ">В НАЛИЧИИ</div>
+                                    <div className="mdl-cell mdl-cell--2-col mdl-cell--1-col-tablet mdl-cell—hide-phone ">СРОК ОЖИДАНИЯ</div>
+                                    <div className="mdl-cell mdl-cell--1-col mdl-cell--1-col-tablet mdl-cell—hide-phone ">ЦЕНА</div>
+                                    <div className="mdl-cell mdl-cell--2-col mdl-cell--2-col-tablet mdl-cell—hide-phone "></div>
                                 </div>
                             </div>
                             <div className="search-result__container">
+                                <div>ПРЕДЛОЖЕНИЯ ПО ОРИГИНАЛЬНЫМ ПРОИЗВОДИТЕЛЯМ</div>
                                 {this.renderContentOriginal(this.props)}
                             </div>
                         </Paper>
