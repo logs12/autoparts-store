887f453bfe4f36309bea153b255279ebee127819
Vasiliy <vasiliys492@gmail.com>
2016-11-25 00:20:57 +0300

    2

---
+++ app/src/frontend/actions/AuthActions.js
@@ -0,0 +1,42 @@
+import {
+    LOGIN_REQUEST,
+    LOGIN_FAIL,
+    LOGIN_SUCCESS,
+    LOGOUT_SUCCESS,
+} from '../../frontend/constants/Auth';
+//import Promise from 'bluebird';
+
+export function login(loginData) {
+    return {
+        types: [
+            LOGIN_REQUEST,
+            LOGIN_FAIL,
+            LOGIN_SUCCESS,
+            LOGOUT_SUCCESS,
+        ],
+        promise: () => {
+            return new Promise(( resolve, reject ) => {
+                fetch('api/user/auth',{
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(loginData)
+                })
+                .then(function(response) {
+                    alert(response.headers.get('Content-Type')); // application/json; charset=utf-8
+                    alert(response.status); // 200
+
+                    return response.json();
+                })
+                .then(function(user) {
+                    alert(user.name); //
+                })
+                .catch((errorMessage) => {
+                    reject(new Error(errorMessage));
+                });
+            });
+        }
+    }
+}
---
+++ app/src/frontend/containers/LoginFormContainer/index.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import LoginForm from "../../components/LoginForm";
+
+import { connect } from 'react-redux';
+import * as AuthActions
+    from '../../actions/AuthActions';
+
+export default class LoginFormContainer extends Component {
+
+    constructor() {
+
+    }
+
+    render() {
+        <LoginForm
+
+        />
+    }
+}
