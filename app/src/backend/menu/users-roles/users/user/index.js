8644bc4f5001842f4ebb518d51e82ba34ae2da7a
Vasiliy <vasiliys492@gmail.com>
2017-02-13 00:16:02 +0300

    2

--- app/src/backend/menu/users-roles/users/user/index.js
+++ app/src/backend/menu/users-roles/users/user/index.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Article from '../../../../../widgets/article/component';
 
+@connect(
+    (state,ownProps) => {
+        return {
+            id: ownProps.params.id,
+            filter: ownProps.location.query.filter
+        };
+    }
+)
 export default class UserView extends Component {
     render() {
-        //debugger;
+        debugger;
         return (
             <Article>{this.props.params.id}
             </Article>
